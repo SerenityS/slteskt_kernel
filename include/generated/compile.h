9f697a074daa1598eb38252e579ebcd56000cf21
SerenityS <jins4218@naver.com>
2015-12-20 15:22:19 +0900

    Make clean

--- include/generated/compile.h
+++
@@ -1,7 +0,0 @@
-/* This file is auto generated, version 1 */
-/* SMP PREEMPT */
-#define UTS_MACHINE "arm"
-#define UTS_VERSION "#1 SMP PREEMPT Thu Nov 12 02:00:02 KST 2015"
-#define LINUX_COMPILE_BY "kitakami"
-#define LINUX_COMPILE_HOST "kitakami"
-#define LINUX_COMPILER "gcc version 4.7.2 20120731 (prerelease) (Linaro GCC 4.7-2012.08) "
